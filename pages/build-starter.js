4fb2ff679a4de2822b8e942009887d25b2609631
Anton <warnes23@yandex.ru>
2020-04-28 15:41:36 +0300

    starter

---
+++ pages/build-starter.js
@@ -0,0 +1,11 @@
+const PATH_MODULE = 'starter';
+
+const ARR_MODULE = [
+    {
+        title: 'Установка',
+        themes: [
+            ['install/settings',  'Настройка'],
+        ],
+    },
+];
+
