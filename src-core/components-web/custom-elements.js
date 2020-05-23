3d4dfdb6a6488a8349cff3654a1a8854db0cac4a
Anton <warnes23@yandex.ru>
2020-05-23 12:55:57 +0300

    z

--- src-core/components-web/custom-elements.js
+++ src-core/components-web/custom-elements.js
@@ -2,12 +2,20 @@
 const customElementRegistry = window.customElements;
 
 const arrCustomElements = [
-	'u-code-text',     'u-code-list',     'u-code-comment',     'u-code',
-	'u-text-define',   'u-text-header',   'u-code-wrapper',     'u-link-wrapper',
-	'u-code-npm',      'u-code-title',    'u-message-ok',       'u-message-info',
-	'u-message-warn',  'u-message-error', 'u-code-accent',      'u-menu',
-	'u-menu-accent',   'u-menu-code',     'u-menu-border',      'u-text-underline',
-	'u-menu-unactive', 'u-text-accent',   'u-block-left-padding'
+	// text
+	'u-text-underline', 'u-text-header', 'u-text-define',
+	// menu
+	'u-menu', 'u-menu-accent', 'u-menu-code', 'u-menu-border', 'u-menu-unactive',
+	// code
+	'u-code', 'u-code-npm', 'u-code-accent', 'u-code-text', 'u-code-list', 'u-code-comment',
+	// link
+	'u-link-wrapper',
+	// message
+	'u-message-ok', 'u-message-info', 'u-message-warn', 'u-message-error',   
+
+	'u-code-wrapper', 'u-code-title',    
+	'u-text-accent',
+	'u-block-left-padding'
 ];
 
 arrCustomElements.forEach(function(element) {
