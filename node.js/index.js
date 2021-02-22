9b204db4473e07e9701968da8de714316f857d92
anton-sergeenkov <45148047+anton-sergeenkov@users.noreply.github.com>
2021-02-23 00:22:57 +0300

    z

--- node.js/index.js
+++ node.js/index.js
@@ -8,72 +8,6 @@ nodeEval(fs.readFileSync('../pages/build-starter.js', 'utf8'));
 let html = '';
 let menu = '';
 
-const getPageHTML = (title, menu, content) => (
-    `
-    <!DOCTYPE html>
-        <html>
-        <head>
-            <title>${title}</title>
-            <meta charset="UTF-8">
-            <link rel="icon" type="image/png" href="../src/img/favicon.png">
-
-            <script src="../src-core/js/utils.js"></script>
-            <!-- Own JavaScript -->
-            <script src="../pages/build-starter.js"></script>
-        </head>
-        <body>
-
-            <div id="container">
-                <div id="menu">
-                    <div id="top">
-                        <a href="#links" class="link">
-                            <div class="img"></div>
-                            <span v-html="'Starter'"></span>
-                        </a>
-                    </div>
-                    ${menu}
-                </div>
-
-                <div id="wrapper">
-                    <div id="links">
-                        <ul>
-                            <slot v-for="itemLink in linksContent">
-                                <li v-if="itemLink">
-                                    <a :href="itemLink.page" :class="'theme-'+itemLink.theme" v-html="itemLink.title"></a>
-                                </li>
-                                <li v-else class="splitter"></li>
-                            </slot>
-                        </ul>
-                    </div>
-                    <div id="content">${content}</div>
-                </div>
-            </div>
-
-            <!-- CSS -->
-            <link rel="stylesheet" type="text/css" href="../src-core/css/basic.css">
-            <link rel="stylesheet" type="text/css" href="../src/css/index.css">
-
-            <!-- Libraries -->
-            <script src="../src-core/libs/highlight/highlight.pack.js"></script>
-            <script src="../src-core/libs/vue.js"></script>
-
-            <!-- Custom Elements -->
-            <script src="../src-core/components-web/custom-elements.js"></script>
-            <link rel="stylesheet" type="text/css" href="../src-core/components-web/custom-elements.css">
-
-            <!-- Vue.js Components -->
-            <script src="../src-core/components-vue/vue-components.js"></script>
-            <link rel="stylesheet" type="text/css" href="../src-core/components-vue/vue-components.css">
-
-            <!-- JavaScript -->
-            <script src="../src-core/js/init.js"></script>
-            <script src="../pages/links.js"></script>
-            <script src="../src/js/build.js"></script>
-        </body>
-        </html>
-    `
-)
-
 const getListHTML = (id, title) => (
     `  
         <li>
@@ -110,6 +44,15 @@ if (!fs.existsSync(PATH_PAGES)) {
     fs.mkdirSync(PATH_PAGES);
 }
 
-const page = getPageHTML('Starter', menu, html);
+
+import { getIndexHTML } from './html/index';
+
+const page = getIndexHTML({
+    up: '../',
+    title: 'Starter',
+    script: 'starter',
+    menu,
+    content: html,
+});
 
 fs.writeFileSync(`${PATH_PAGES}/${PATH_MODULE}.html`, page);
