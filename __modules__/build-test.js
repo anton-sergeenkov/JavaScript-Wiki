24427462db2f6325f5b63e679bffae65f3bbf993
anton-sergeenkov <45148047+anton-sergeenkov@users.noreply.github.com>
2021-02-25 19:16:17 +0300

    starter

---
+++ __modules__/build-test.js
@@ -0,0 +1,16 @@
+ARR_MODULE = [
+    {
+        title: 'Первая секция',
+        themes: [
+            ['test/first/one',  'One'],
+            ['test/first/two',  'Two'],
+        ]
+    },
+    {
+        title: 'Вторая секция',
+        themes: [
+            ['test/second/one',  'One'],
+            ['test/second/two',  'Two'],
+        ]
+    },
+];
